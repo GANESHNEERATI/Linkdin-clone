7145f70d4d4f5226ec49a34b6fc6f4efdb5759d9
GANESHNEERATI <40731974+GANESHNEERATI@users.noreply.github.com>
2020-12-29 12:18:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <h2>welcome</h2>
+    </div>
+  );
+}
